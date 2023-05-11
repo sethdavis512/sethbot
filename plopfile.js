6e9edf7ba5cad55501712ca49a3d3df4fffa4852
Seth Davis <sethdavis512@gmail.com>
2023-05-11 13:27:03 -0500

    Cleanup the cruft

--- plopfile.js
+++ plopfile.js
@@ -38,23 +38,78 @@ export default function ({ setGenerator }) {
         actions: buildAddManyActions('context')
     });
 
+    setGenerator('grab-and-go-prisma-schema', {
+        description: 'Grab and go Prisma schema template',
+        prompts: defaultPrompts,
+        actions: [
+            {
+                type: 'addMany',
+                destination: `${process.cwd()}/`,
+                templateFiles: `plop-templates/grab-and-go-prisma-schema/**/*.hbs`,
+                base: `plop-templates/grab-and-go-prisma-schema`
+            }
+        ]
+    });
+
     setGenerator('hook', {
         description: 'Typescript hook with unit test',
         prompts: defaultPrompts,
         actions: buildAddManyActions('hook')
     });
 
-    setGenerator('use-state-machine', {
-        description: 'Typescript useStateMachine hook',
-        prompts: [],
-        actions: [
+    setGenerator('remix-route', {
+        description: 'Remix route in Typescript',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Name'
+            },
+            {
+                type: 'confirm',
+                name: 'hasLoader',
+                message: 'Loader',
+                default: true
+            },
+            {
+                type: 'confirm',
+                name: 'hasAction',
+                message: 'Action',
+                default: true
+            },
+            {
+                type: 'confirm',
+                name: 'hasMeta',
+                message: 'Meta',
+                default: false
+            },
+            {
+                type: 'confirm',
+                name: 'hasLinks',
+                message: 'Links',
+                default: false
+            },
+            {
+                type: 'confirm',
+                name: 'hasErrorBoundary',
+                message: 'Error boundary',
+                default: false
+            }
+        ],
+        actions: () => [
             {
                 type: 'addMany',
-                destination: `${process.cwd()}/`,
-                templateFiles: 'plop-templates/use-state-machine/**/*.hbs',
-                base: 'plop-templates/use-state-machine'
+                destination: process.cwd(),
+                templateFiles: 'plop-templates/remix-route/**/*.hbs',
+                base: 'plop-templates/remix-route'
             }
         ]
+    });
+
+    setGenerator('tiny-react', {
+        description: 'Tiny React app built using Parcel',
+        prompts: defaultPrompts,
+        actions: buildAddManyActions('tiny-react', 'kebabCase')
     });
 
     setGenerator('use-local-storage', {
@@ -71,72 +126,16 @@ export default function ({ setGenerator }) {
         ]
     });
 
-    setGenerator('remix-route', {
-        description: 'Remix route in Typescript',
-        prompts: defaultPrompts.concat([
-            {
-                type: 'input',
-                name: 'routeFilePath',
-                message: 'Where',
-                default: (answers) =>
-                    `${process.cwd()}/${answers.name.toLowerCase()}`
-            },
-            {
-                type: 'confirm',
-                name: 'hasLoader',
-                message: 'Loader'
-            },
-            {
-                type: 'confirm',
-                name: 'hasAction',
-                message: 'Action'
-            },
-            {
-                type: 'confirm',
-                name: 'hasMeta',
-                message: 'Meta'
-            },
-            {
-                type: 'confirm',
-                name: 'hasLinks',
-                message: 'Links'
-            }
-        ]),
-        actions: (answers) => [
-            {
-                type: 'addMany',
-                destination: `${
-                    answers.routeFilePath || process.cwd()
-                }/{{ kebabCase name }}/`,
-                templateFiles: 'plop-templates/remix-route/**/*.hbs',
-                base: 'plop-templates/remix-route'
-            }
-        ]
-    });
-
-    setGenerator('remix-airtable-crud', {
-        description: 'Remix Airtable CRUD',
-        prompts: [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Model (singular, i.e. Player)'
-            }
-        ],
+    setGenerator('use-state-machine', {
+        description: 'Typescript useStateMachine hook',
+        prompts: [],
         actions: [
             {
                 type: 'addMany',
                 destination: `${process.cwd()}/`,
-                templateFiles: `plop-templates/remix-airtable-crud/**/*.hbs`,
-                base: `plop-templates/remix-airtable-crud`,
-                force: true
+                templateFiles: 'plop-templates/use-state-machine/**/*.hbs',
+                base: 'plop-templates/use-state-machine'
             }
         ]
-    });
-
-    setGenerator('tiny-react', {
-        description: 'Tiny React app built using Parcel',
-        prompts: defaultPrompts,
-        actions: buildAddManyActions('tiny-react', 'kebabCase')
     });
 }
